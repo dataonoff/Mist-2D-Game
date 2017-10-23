fbbf81f43167f15032b3e8ecf04c19d3ea8f7d08
dataonoff <charlilounet3@hotmail.com>
2017-10-23 18:31:39 +0200

    Display Map

--- src/main/java/WindowGame.java
+++ src/main/java/WindowGame.java
@@ -1,16 +1,24 @@
 import org.newdawn.slick.*;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class WindowGame extends BasicGame{
 
-    GameContainer container;
+    private GameContainer container;
+    private TiledMap map;
 
     public WindowGame(String title) {
         super("yo");
     }
 
+    public static void main(String[] args) throws SlickException {
+        new AppGameContainer(new WindowGame("Lolo et charlie Game"), 640, 480, false).start();
+    }
+
     @Override
     public void init(GameContainer container) throws SlickException {
         this.container = container;
+        this.map = new TiledMap("map/tecar.tmx");
+
     }
 
     public void update(GameContainer gameContainer, int i) throws SlickException {
@@ -18,12 +26,9 @@ public class WindowGame extends BasicGame{
     }
 
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-
+        this.map.render(0,0);
     }
 
-    public static void main(String[] args) throws SlickException {
-        new AppGameContainer(new WindowGame("Lolo et charlie Game"), 640, 480, false).start();
-    }
 
     @Override
     public void keyReleased(int key, char c) {
