b369077391380132ce43d4e22112d44770083391
dataonoff <charlilounet3@hotmail.com>
2017-10-23 23:11:55 +0200

    Display Map with Mainscreen

--- src/main/resources/map/tuiles/player.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="player" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="Hero.png" trans="ff00ff" width="101" height="101"/>
-</tileset>
